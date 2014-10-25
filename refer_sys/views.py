72f3bcbacab5c8ec815f53c206894b88ead1dd08
brandoshmando <brancraft@gmail.com>
2014-10-25 17:16:12 -0400

    root page coming along nicely

--- refer_sys/views.py
+++ refer_sys/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-
+from refer_sys.models import Link
 def root_page(request):
-  return render (request, 'refer_sys/root_page.html', {})
+  links = Link.objects.order_by("created_at")
+  return render (request, 'refer_sys/root_page.html',
+    {'links': links}
+  )
