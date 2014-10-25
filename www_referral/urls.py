23bd0af36b1c568da330cda915391b1ce9332e00
brandoshmando <brancraft@gmail.com>
2014-10-25 14:24:26 -0400

    created layout for urls within refer_sys. root_url is in place

--- www_referral/urls.py
+++ www_referral/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'www_referral.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('refer_sys.urls')),
 )
