adb0faa45365704a5bb8ea4a4292778893a66091
brandoshmando <brancraft@gmail.com>
2014-10-25 13:42:33 -0400

    Started creating Link model

--- refer_sys/models.py
+++ refer_sys/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Link(models.Model):
+  title = models.CharField(max_length=200)
+  clicks = models.IntegerField()
+
