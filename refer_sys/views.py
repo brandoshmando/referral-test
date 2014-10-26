975a60606c97d6d5ad44a302ce5bb9d65a123358
brandoshmando <brancraft@gmail.com>
2014-10-26 11:54:47 -0400

    Added link form and imported it on the view

--- refer_sys/views.py
+++ refer_sys/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from refer_sys.models import Link
+from .forms import LinkForm
 def root_page(request):
   links = Link.objects.order_by("created_at")
+  form = LinkForm()
   return render (request, 'refer_sys/root_page.html',
     {'links': links}
   )
+# def link_create(request)
+#   form =
