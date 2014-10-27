977d62b950e0573094ac8fa8be97decf68bd523e
brandoshmando <brancraft@gmail.com>
2014-10-27 10:34:25 -0400

    Final commit

--- refer_sys/forms.py
+++ refer_sys/forms.py
@@ -5,6 +5,8 @@ class LinkForm(forms.ModelForm):
   class Meta:
     model = Link
     fields = ('title',)
+    def clean_name(self):
+        return self.cleaned_data["title"].lower()
 
 class DeleteLinkForm(forms.ModelForm):
   class Meta:
